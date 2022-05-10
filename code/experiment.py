e8c04a0de5465799c03e3093788d1054d20ae49d
Karol Dobiczek <k.t.dobiczek@student.tudelft.nl>
2022-05-10 03:28:51 +0200

    move methods to class, enable online model use

--- code/experiment.py
+++ code/experiment.py
@@ -1,4 +1,6 @@
 import warnings
+from copy import deepcopy
+
 import imageio
 import timeit
 import numpy as np
@@ -6,7 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
-from carla.data.catalog import CsvCatalog
+from carla.data.catalog import CsvCatalog, OnlineCatalog
 from carla import MLModelCatalog
 from carla.recourse_methods import Clue, Wachter
 from carla.models.negative_instances import predict_negative_instances
@@ -14,7 +16,7 @@ from carla.evaluation.benchmark import Benchmark
 from sklearn.metrics import f1_score, accuracy_score
 from sklearn.cluster import KMeans
 from sklearn import metrics
-from recourse_util import update_dataset, predict, print_scores
+from recourse_util import update_dataset, predict
 
 warnings.filterwarnings("ignore")
 
@@ -24,7 +26,7 @@ def train_model(dataset):
 
     model = MLModelCatalog(
         dataset,
-        model_type="ann",
+        model_type="linear",
         load_online=False,
         backend="pytorch"
     )
@@ -40,21 +42,6 @@ def train_model(dataset):
     return model
 
 
-def load_dataset():
-    dataset = CsvCatalog(
-        #         file_path='datasets/bimodal_dataset_1.csv',
-        #         file_path='datasets/unimodal_dataset_1.csv',
-        file_path='datasets/unimodal_dataset_2.csv',
-        categorical=[],
-        continuous=['feature1', 'feature2'],
-        immutables=[],
-        target='target'
-    )
-
-    data_name = 'custom'
-    return dataset
-
-
 def train_recourse_method(method, model, dataset=None, data_name=None, hyperparams=None):
     rm = None
     if method == "clue":
@@ -64,7 +51,7 @@ def train_recourse_method(method, model, dataset=None, data_name=None, hyperpara
             "width": 10,
             "depth": 3,
             "latent_dim": 12,
-            "batch_size": 4,
+            "batch_size": 20,
             "epochs": 5,
             "lr": 0.0001,
             "early_stop": 20,
@@ -85,8 +72,8 @@ def train_recourse_method(method, model, dataset=None, data_name=None, hyperpara
     return rm
 
 
-def draw(data):
-    plt.scatter(data['feature1'], data['feature2'], c=data['target'])
+def draw(data, features, target):
+    plt.scatter(data[features[0]], data[features[1]], c=data[target])
     plt.show()
 
 
@@ -102,34 +89,6 @@ def get_factuals(dataset, sample_num=5, max_m_iter=3):
         m_iter += 1
 
     return model, factuals
-
-
-def execute_experiment_iteration(method, dataset, model, factuals, results, draw_state=False):
-    print("Number of factuals", len(factuals))
-
-    # add_data_statistics(model, dataset, results)
-
-    if method == 'clue':
-        rm = train_recourse_method('clue', model, dataset, data_name='custom')
-    else:
-        rm = train_recourse_method('wachter', model)
-
-    start = timeit.default_timer()
-    counterfactuals = rm.get_counterfactuals(factuals)
-    stop = timeit.default_timer()
-    print("Number of counterfactuals:", len(counterfactuals.dropna()))
-
-    update_dataset(dataset, factuals, counterfactuals)
-
-    benchmark = CustomBenchmark(model, rm, factuals, counterfactuals, stop - start)
-    results['benchmark'] = benchmark.run_benchmark()
-
-    add_data_statistics(model, dataset, results)
-
-    if draw_state:
-        draw(dataset._df)
-
-    return dataset
 
 
 def get_empty_results():
@@ -178,7 +137,6 @@ class CustomBenchmark(Benchmark):
         self._factuals = factuals.copy()
         self._counterfactuals = counterfactuals.copy()
         self._counterfactuals.index = self._factuals.index.copy()
-        # self._counterfactuals = pd.DataFrame(counterfactuals.to_numpy(), columns=counterfactuals.columns, index=factuals.copy().index)
         self._timer = timer
 
         # Avoid using scaling and normalizing more than once
@@ -186,22 +144,29 @@ class CustomBenchmark(Benchmark):
             self._mlmodel.use_pipeline = False  # type: ignore
 
 
-class Experiment():
+class Experiment:
     def __init__(self):
         self.num = 10
         self._iter_id = 0
+        self._data_path = 'datasets/bimodal_dataset_1.csv'
+
+        self._features = []
+        self._dataset_name = None
+        self._dataset = None
 
         self.results = {}
 
     def run_experiment(self):
-        self._iter_id += 1
-        dataset = load_dataset()
+        if not self._dataset:
+            print("Load a dataset before running experiments")
 
-        clue_dataset = load_dataset()
+        self._iter_id += 1
+
+        clue_dataset = deepcopy(self._dataset)
         clue_result = get_empty_results()
         self.results['clue'] = clue_result
 
-        wachter_dataset = load_dataset()
+        wachter_dataset = deepcopy(self._dataset)
         wachter_result = get_empty_results()
         self.results['wachter'] = wachter_result
 
@@ -212,21 +177,66 @@ class Experiment():
             clue_model, clue_factuals = get_factuals(clue_dataset, sample_num=samples)
             wachter_model, wachter_factuals = get_factuals(wachter_dataset, sample_num=samples)
 
-            factuals = pd.merge(clue_factuals, wachter_factuals, how='inner', on=[*dataset.continuous, dataset.target])
-            factuals = pd.merge(factuals, dataset._df, how='inner', on=dataset.continuous)
+            factuals = pd.merge(clue_factuals, wachter_factuals, how='inner', on=list(self._dataset._df.columns))
+            factuals = pd.merge(factuals, self._dataset._df, how='inner', on=list(self._dataset._df.columns))
 
             if len(factuals) > samples:
                 factuals = factuals.sample(samples)
 
-            execute_experiment_iteration('clue', clue_dataset, clue_model, factuals, clue_result)
-            execute_experiment_iteration('wachter', wachter_dataset, wachter_model, factuals, wachter_result)
+            self.execute_experiment_iteration('clue', clue_dataset, clue_model, factuals, clue_result)
+            self.execute_experiment_iteration('wachter', wachter_dataset, wachter_model, factuals, wachter_result)
 
-    def generate_animation(self, results, method='clue'):
+    def execute_experiment_iteration(self, method, dataset, model, factuals, results, draw_state=False):
+        print("Number of factuals", len(factuals))
+
+        if method == 'clue':
+            rm = train_recourse_method('clue', model, dataset, data_name=self._dataset_name)
+        else:
+            rm = train_recourse_method('wachter', model)
+
+        start = timeit.default_timer()
+        counterfactuals = rm.get_counterfactuals(factuals)
+        stop = timeit.default_timer()
+        print("Number of counterfactuals:", len(counterfactuals.dropna()))
+
+        update_dataset(dataset, factuals, counterfactuals)
+
+        benchmark = CustomBenchmark(model, rm, factuals, counterfactuals, stop - start)
+        results['benchmark'].append(benchmark.run_benchmark())
+
+        add_data_statistics(model, dataset, results)
+
+        if draw_state:
+            draw(dataset._df, self._features[:2], self._dataset.target)
+
+        return dataset
+
+    def load_dataset(self, name, **kwargs):
+        if name == 'custom':
+            dataset = CsvCatalog(
+                file_path=kwargs.pop('path'),
+                categorical=kwargs.pop('categorical', []),
+                continuous=kwargs.pop('continuous', []),
+                immutables=kwargs.pop('immutables', []),
+                target=kwargs.pop('target')
+            )
+        else:
+            dataset = OnlineCatalog(name)
+
+        self._dataset_name = name
+        self._features = [*dataset.continuous, *dataset.categorical]
+        self._dataset = dataset
+
+    def generate_animation(self, results, method='clue', features=None):
         data = results[method]['datasets']
+
+        if not features:
+            features = self._features[:2]
+
         names = [f"images/{method}{str(n)}.png" for n in range(len(data))]
 
         for i, name in enumerate(names):
-            plt.scatter(data[i]['feature1'], data[i]['feature2'], c=data[i]['target'])
+            plt.scatter(data[i][features[0]], data[i][features[1]], c=data[i][self._dataset.target])
             plt.savefig(name)
             plt.close()
 
@@ -234,7 +244,7 @@ class Experiment():
 
         with imageio.get_writer(f'{gif_path}', mode='I') as writer:
             for filename in names:
-                image = imageio.imread(filename)
+                image = imageio.v3.imread(filename)
                 writer.append_data(image)
 
         print(f"Saved gif to {gif_path}")
@@ -249,6 +259,7 @@ class Experiment():
 
 if __name__ == "__main__":
     experiment = Experiment()
+    experiment.load_dataset("custom", path='datasets/bimodal_dataset_1.csv', continuous=['feature1', 'feature2'], target='target')
     experiment.run_experiment()
     experiment.save_gifs()
     print(experiment.results)
